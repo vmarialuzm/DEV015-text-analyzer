adc842f4bf3d202c4c42e795a7da945b3b02d90e
vmarialuzm <vmarialuzm@gmail.com>
2024-08-18 17:19:19 -0500

    manejo del DOM e integracion con las funciones

--- src/analyzer.js
+++ src/analyzer.js
@@ -3,12 +3,12 @@ const analyzer = {
   getWordCount: (text) => {
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
     let count = 0;
-    const words = text.split(' ');
+    const words = text.trim().split(' ');
 
     for (let i = 0; i < words.length; i++) {
       count += 1
     }
-    console.log("getWordCount", count)
+    return count
   },
 
   getCharacterCount: (text) => {
@@ -18,7 +18,7 @@ const analyzer = {
     for (let i = 0; i < text.length; i++) {
       count += 1
     }
-    console.log("getCharacterCount", count)
+    return count
   },
   
   getCharacterCountExcludingSpaces: (text) => {
@@ -29,7 +29,7 @@ const analyzer = {
     for (let i = 0; i < text.length; i++) {
       count += 1
     }
-    console.log("getCharacterCountExcludingSpaces", count)
+    return count
   },
   
   getAverageWordLength: (text) => {    
@@ -50,7 +50,7 @@ const analyzer = {
       sum_long_words += long_word
     }
     average_words = sum_long_words/count_words
-    console.log("getAverageWordLength", average_words.toFixed(2))
+    return average_words.toFixed(2)
   },
 
   getNumberCount: (text) => {
@@ -63,7 +63,7 @@ const analyzer = {
         count_number += 1
       }
     }
-    console.log("getNumberCount", count_number)
+    return count_number
   },
 
   getNumberSum: (text) => {
@@ -78,7 +78,7 @@ const analyzer = {
         sum_number += parseInt(text[i])
       }
     }
-    console.log("getNumberSum", sum_number)
+    return sum_number
   },
 };
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,43 @@
 import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
-analyzer.getWordCount("Camina un paso hacia el este")
-analyzer.getCharacterCount("Camina un paso hacia el este 1 2")
-analyzer.getCharacterCountExcludingSpaces("Camina un paso hacia el este")
-analyzer.getAverageWordLength("Camina un paso hacia el este")
-analyzer.getNumberCount("Camina un paso hacia el este 1 20")
-analyzer.getNumberSum("Camina un paso hacia el este 1357")
+
+
+const cuadro_texto = document.querySelector("textarea[name='user-input']");
+const boton_limpiar = document.getElementById("reset-button");
+let li_metricas_caracteres = document.querySelector(".metricas.caracteres")
+let li_metricas_sin_espacios = document.querySelector(".metricas.sin.espacios")
+let li_metricas_palabras = document.querySelector(".metricas.palabras")
+let li_metricas_numeros = document.querySelector(".metricas.numeros")
+let li_metricas_sum_numeros = document.querySelector(".metricas.sum.numeros")
+let li_metricas_promedio = document.querySelector(".metricas.promedio")
+
+cuadro_texto.addEventListener("input", () => {
+    
+    const CharacterCount = analyzer.getCharacterCount(cuadro_texto.value)
+    const CharacterCountExcludingSpaces = analyzer.getCharacterCountExcludingSpaces(cuadro_texto.value)
+    const WordCount = analyzer.getWordCount(cuadro_texto.value)
+    const NumberCount = analyzer.getNumberCount(cuadro_texto.value)
+    const NumberSum = analyzer.getNumberSum(cuadro_texto.value)
+    const AverageWordLength = analyzer.getAverageWordLength(cuadro_texto.value)
+    
+    li_metricas_caracteres.innerHTML = `Caracteres: <span> ${CharacterCount} </span>`
+    li_metricas_sin_espacios.innerHTML = `Caracteres Sin Espacios: <span> ${CharacterCountExcludingSpaces} </span>`
+    li_metricas_palabras.innerHTML = `Palabras: <span> ${WordCount} </span>`
+    li_metricas_numeros.innerHTML = `Números: <span> ${NumberCount} </span>`
+    li_metricas_sum_numeros.innerHTML = `Suma números: <span> ${NumberSum} </span>`
+    li_metricas_promedio.innerHTML = `Promedio longitud: <span> ${AverageWordLength} </span>`
+});
+
+
+
+/* Limpia todo el texto de textaarea */
+boton_limpiar.addEventListener("click", () => {
+    cuadro_texto.value = "";
+
+    // Establece todos los spans a 0
+    const span_metricas = document.querySelectorAll("li.metricas span")
+    span_metricas.forEach(span => span.textContent = "0")
+});
+
+
