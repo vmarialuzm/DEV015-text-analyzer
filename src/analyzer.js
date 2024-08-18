0dc19c0b430e81b032afa4a8eb8d2323e1787c7b
vmarialuzm <vmarialuzm@gmail.com>
2024-08-18 11:15:32 -0500

    termine las 6 funciones, corren bien los test criterios minimos de aceptación

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,4 +1,5 @@
 const analyzer = {  
+
   getWordCount: (text) => {
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
     let count = 0;
@@ -7,23 +8,77 @@ const analyzer = {
     for (let i = 0; i < words.length; i++) {
       count += 1
     }
-    console.log("function getWordCount", count)
-
+    console.log("getWordCount", count)
   },
+
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+    let count = 0;
+
+    for (let i = 0; i < text.length; i++) {
+      count += 1
+    }
+    console.log("getCharacterCount", count)
   },
+  
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+    let count = 0;
+    text = text.replaceAll(" ", "");
+
+    for (let i = 0; i < text.length; i++) {
+      count += 1
+    }
+    console.log("getCharacterCountExcludingSpaces", count)
   },
+  
   getAverageWordLength: (text) => {    
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    let count_words = 0;
+    let word = ''
+    let long_word = 0
+    let sum_long_words = 0
+    let average_words = 0
+
+    const words = text.split(' ');
+
+    for (let i = 0; i < words.length; i++) {
+      count_words += 1
+      word = words[i]
+      long_word = word.length
+
+      sum_long_words += long_word
+    }
+    average_words = sum_long_words/count_words
+    console.log("getAverageWordLength", average_words.toFixed(2))
   },
+
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+    let count_number = 0
+    text = text.replaceAll(" ", "")
+
+    for (let i = 0; i < text.length; i++) {
+      if (!isNaN(parseInt(text[i]))) {
+        count_number += 1
+      }
+    }
+    console.log("getNumberCount", count_number)
   },
+
   getNumberSum: (text) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+    let count_number = 0
+    let sum_number = 0
+    text = text.replaceAll(" ", "")
+
+    for (let i = 0; i < text.length; i++) {
+      if (!isNaN(parseInt(text[i]))) {
+        count_number += 1
+        sum_number += parseInt(text[i])
+      }
+    }
+    console.log("getNumberSum", sum_number)
   },
 };
 
--- src/index.js
+++ src/index.js
@@ -2,3 +2,8 @@ import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
 analyzer.getWordCount("Camina un paso hacia el este")
+analyzer.getCharacterCount("Camina un paso hacia el este 1 2")
+analyzer.getCharacterCountExcludingSpaces("Camina un paso hacia el este")
+analyzer.getAverageWordLength("Camina un paso hacia el este")
+analyzer.getNumberCount("Camina un paso hacia el este 1 20")
+analyzer.getNumberSum("Camina un paso hacia el este 1357")
