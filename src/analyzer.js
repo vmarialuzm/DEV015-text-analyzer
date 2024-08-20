f72251702c0e0cb2c800d62709f3f909f91c370a
vmarialuzm <vmarialuzm@gmail.com>
2024-08-19 22:33:57 -0500

    ya corren los test unitarios y e2e, cambie el nombre de las variables a camelCase

--- src/analyzer.js
+++ src/analyzer.js
@@ -24,7 +24,9 @@ const analyzer = {
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
     let count = 0;
-    text = text.replaceAll(" ", "");
+    const regex = /[^\w]|_/g;
+
+    text = text.replace(regex, "");
 
     for (let i = 0; i < text.length; i++) {
       count += 1
@@ -55,29 +57,29 @@ const analyzer = {
 
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
-    let count_number = 0
-    text = text.replaceAll(" ", "")
-
-    for (let i = 0; i < text.length; i++) {
-      if (!isNaN(parseInt(text[i]))) {
-        count_number += 1
-      }
+    const regex = /\b\d+(\.\d+)?\b/g ;
+    const result = text.match(regex);
+    
+    if (result) {
+      return result.length
+    } else {
+      return 0
     }
-    return count_number
   },
 
   getNumberSum: (text) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
     let sum_number = 0
-    text = text.replaceAll(" ", "")
+    const regex = /\b\d+(\.\d+)?\b/g ;
+    const result = text.match(regex);
 
-    for (let i = 0; i < text.length; i++) {
-      if (!isNaN(parseInt(text[i]))) {
-        sum_number += parseInt(text[i])
-      }
+    if (result) {
+      result.forEach(element => sum_number += parseFloat(element));
+      return sum_number;
+    } else {
+      return 0
     }
-    return sum_number
-  },
+  }
 };
 
 export default analyzer;
--- src/index.js
+++ src/index.js
@@ -3,41 +3,41 @@ import analyzer from './analyzer.js';
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
 
 
-const cuadro_texto = document.querySelector("textarea[name='user-input']");
-const boton_limpiar = document.getElementById("reset-button");
-const li_metricas_caracteres = document.querySelector(".metricas.caracteres")
-const li_metricas_sin_espacios = document.querySelector(".metricas.sin.espacios")
-const li_metricas_palabras = document.querySelector(".metricas.palabras")
-const li_metricas_numeros = document.querySelector(".metricas.numeros")
-const li_metricas_sum_numeros = document.querySelector(".metricas.sum.numeros")
-const li_metricas_promedio = document.querySelector(".metricas.promedio")
+const cuadroTexto = document.querySelector("textarea[name='user-input']");
+const botonLimpiar = document.getElementById("reset-button");
+const liMetricasCaracteres = document.querySelector(".metricas.caracteres")
+const liMetricasSinEspacios = document.querySelector(".metricas.sin.espacios")
+const liMetricasPalabras = document.querySelector(".metricas.palabras")
+const liMetricasNumeros = document.querySelector(".metricas.numeros")
+const liMetricasSumNumeros = document.querySelector(".metricas.sum.numeros")
+const liMetricasPromedio = document.querySelector(".metricas.promedio")
 
-cuadro_texto.addEventListener("input", () => {
+cuadroTexto.addEventListener("input", () => {
     
-  const CharacterCount = analyzer.getCharacterCount(cuadro_texto.value)
-  const CharacterCountExcludingSpaces = analyzer.getCharacterCountExcludingSpaces(cuadro_texto.value)
-  const WordCount = analyzer.getWordCount(cuadro_texto.value)
-  const NumberCount = analyzer.getNumberCount(cuadro_texto.value)
-  const NumberSum = analyzer.getNumberSum(cuadro_texto.value)
-  const AverageWordLength = analyzer.getAverageWordLength(cuadro_texto.value)
+  const characterCount = analyzer.getCharacterCount(cuadroTexto.value)
+  const characterCountExcludingSpaces = analyzer.getCharacterCountExcludingSpaces(cuadroTexto.value)
+  const wordCount = analyzer.getWordCount(cuadroTexto.value)
+  const numberCount = analyzer.getNumberCount(cuadroTexto.value)
+  const numberSum = analyzer.getNumberSum(cuadroTexto.value)
+  const averageWordLength = analyzer.getAverageWordLength(cuadroTexto.value)
     
-  li_metricas_caracteres.innerHTML = `Caracteres: <span> ${CharacterCount} </span>`
-  li_metricas_sin_espacios.innerHTML = `Caracteres Sin Espacios: <span> ${CharacterCountExcludingSpaces} </span>`
-  li_metricas_palabras.innerHTML = `Palabras: <span> ${WordCount} </span>`
-  li_metricas_numeros.innerHTML = `Números: <span> ${NumberCount} </span>`
-  li_metricas_sum_numeros.innerHTML = `Suma números: <span> ${NumberSum} </span>`
-  li_metricas_promedio.innerHTML = `Promedio longitud: <span> ${AverageWordLength} </span>`
+  liMetricasCaracteres.innerHTML = `Caracteres: <span> ${characterCount} </span>`
+  liMetricasSinEspacios.innerHTML = `Caracteres Sin Espacios: <span> ${characterCountExcludingSpaces} </span>`
+  liMetricasPalabras.innerHTML = `Palabras: <span> ${wordCount} </span>`
+  liMetricasNumeros.innerHTML = `Números: <span> ${numberCount} </span>`
+  liMetricasSumNumeros.innerHTML = `Suma números: <span> ${numberSum} </span>`
+  liMetricasPromedio.innerHTML = `Promedio longitud: <span> ${averageWordLength} </span>`
 });
 
 
 
 /* Limpia todo el texto de textaarea */
-boton_limpiar.addEventListener("click", () => {
-  cuadro_texto.value = "";
+botonLimpiar.addEventListener("click", () => {
+  cuadroTexto.value = "";
 
   // Establece todos los spans a 0
-  const span_metricas = document.querySelectorAll("li.metricas span")
-  span_metricas.forEach(span => span.textContent = "0")
+  const spanMetricas = document.querySelectorAll("li.metricas span")
+  spanMetricas.forEach(span => span.textContent = "0")
 });
 
 /* la funcion de contar palabras, tambien cuenta numeros */
